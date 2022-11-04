6d50a4748ff910da3b363cc48f35211e3cf7a51c
sumit <sumitnath.edu@gmai.com>
2022-11-04 14:03:30 +0530

    =set up amount and createdat formating

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 //import { connect } from 'react-redux';
 // we donot need the redux store as we have dont have remove here, so we remove connect()
 // challenge remove remove button from  the ExpenseListItem.js and add it into EditExpensePage.js
@@ -7,18 +9,18 @@ import { Link } from "react-router-dom";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
 
-        <
-        div >
-        <
-        Link to = { `/edit/${id}` } >
-        <
-        h3 > { description } < /h3> <
-        /Link> <
-        p > { amount }--{ createdAt } < /p>
+        <div>
+        <Link to = { `/edit/${id}` } >
+        <h3> { description } </h3>
+         </Link> 
+         <p> {numeral(amount/1000).format('$0,0.00' )
+
+         }
+         
+         -- {moment(createdAt).format('MMMMM Do, YYYY') } </p>
 
 
-        <
-        /div>
+        </div>
 
     )
     //export default connect()(ExpenseListItem)
